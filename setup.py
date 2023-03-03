7fdba3532c996d6bd54f0918e56cbe8c0d51b1a0
Wenchao Lin <linwenchao@yeah.net>
2023-03-03 18:49:03 +0800

    add pyproject.toml

--- setup.py
+++ setup.py
@@ -1,22 +1,16 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 file_path = 'README.md'
 
 setup(name='assemblyStatistics',
-      version='1.1.2',
+      version='1.1.3',
       description='A script to evaluate the assembly of a given genome.',
       long_description=open(file_path, encoding='utf-8').read(),
       long_description_content_type='text/markdown',
-      license='GPL-3',
       author='Wenchao Lin',
       include_package_data=True,
       author_email='linwenchao@yeah.net',
       url='https://github.com/WenchaoLin/assemblyStatistics',
-      packages=['.'],
+      packages = find_packages(),
       keywords=['bioinformatics', 'genome statics', 'scaffold N50'],
-      entry_points={
-        'console_scripts': [
-            'assemblyStatistics = src.assemblyStatistics:main'
-        ]
-      }
       )
